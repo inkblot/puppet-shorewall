9aefa22fde1fd3e0017a82af49f2a6e639f38859
Nate Riffe <inkblot@movealong.org>
2012-07-31 08:01:13 -0500

    Fix tunnels for real

--- manifests/multi.pp
+++ manifests/multi.pp
@@ -64,20 +64,30 @@ class shorewall::multi (
 		$zone,
 		$gateway = '0.0.0.0/0',
 	) {
-		if $ipv4_tunnels and ( $proto == 'ipv4' ) {
-			concat::fragment { "tunnel-ipv4-${type}-${gateway}":
-				order   => '50',
-				target  => '/etc/shorewall/tunnels',
-				content => "${type} ${zone} ${gateway}\n",
+		case $proto {
+			'ipv4': {
+				if $shorewall::multi::ipv4_tunnels {
+					concat::fragment { "tunnel-ipv4-${type}-${gateway}":
+						order   => '50',
+						target  => '/etc/shorewall/tunnels',
+						content => "${type} ${zone} ${gateway}\n",
+					}
+				} else {
+					fail('An ipv4 tunnel has been defined but \'ipv4_tunnels\' is false')
+				}
 			}
-		}
-
-		if $ipv6_tunnels and ( $proto == 'ipv6' ) {
-			concat::fragment { "tunnel-ipv6-${type}-${gateway}":
-				order   => '50',
-				target  => '/etc/shorewall6/tunnels',
-				content => "${type} ${zone} ${gateway}\n",
+			'ipv6': {
+				if $shorewall::multi::ipv6_tunnels {
+					concat::fragment { "tunnel-ipv6-${type}-${gateway}":
+						order   => '50',
+						target  => '/etc/shorewall6/tunnels',
+						content => "${type} ${zone} ${gateway}\n",
+					}
+				} else {
+					fail('An ipv6 tunnel has been defined been \'ipv6_tunnels\' is false')
+				}
 			}
+			default: { fail("Unknown value for 'proto': ${proto}") }
 		}
 	}
 
