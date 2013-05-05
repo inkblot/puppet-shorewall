bf6b68aedb1719c94a569892d5a4c9bd2a23abf0
Nate Riffe <inkblot@movealong.org>
2013-05-05 18:35:14 -0500

    Fix syntax

--- manifests/multi/host.pp
+++ manifests/multi/host.pp
@@ -1,8 +1,8 @@
 define shorewall::multi::host (
-	$ipv4_cidr
-	$ipv6_prefix
+	$ipv4_cidr   = '',
+	$ipv6_prefix = '',
 ) {
-	if $ipv4 {
+	if $ipv4_cidr != '' {
 		concat::fragment { "shorewall-host-ipv4-${name}":
 			order   => '50',
 			target  => '/etc/shorewall/hosts',
@@ -10,7 +10,7 @@ define shorewall::multi::host (
 		}
 	}
 
-	if $ipv6 {
+	if $ipv6_prefix != '' {
 		concat::fragment { "shorewall-host-ipv6-${name}":
 			order   => '50',
 			target  => '/etc/shorewall6/hosts',
