eda8924282cdc762085816d8ea1c038b2000675b
Nate Riffe <inkblot@movealong.org>
2013-05-05 18:54:57 -0500

    Put the newline in the right place

--- manifests/multi/masq.pp
+++ manifests/multi/masq.pp
@@ -7,7 +7,7 @@ define shorewall::multi::masq (
 	concat::fragment { "masq-${name}":
 		order   => '50',
 		target  => '/etc/shorewall/masq',
-		content => inline_template("<% sources.each { |source| %><%= name %> <%= source %><% } %>\n"),
+		content => inline_template("<% sources.each { |source| %><%= name %> <%= source %>\n<% } %>"),
 	}
 
 }	
