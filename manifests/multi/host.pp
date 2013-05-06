e95835b6f1d9e95c274595b3a885b9f997e43a87
Nate Riffe <inkblot@movealong.org>
2013-05-05 19:49:03 -0500

    Fix syntax error

--- manifests/multi/host.pp
+++ manifests/multi/host.pp
@@ -3,12 +3,10 @@ define shorewall::multi::host (
 	$ipv6_prefix = '',
 ) {
 	if $ipv4_cidr != '' {
-		notify { "ipv4_cidr = '${ipv4_cidr}'": }
-
 		concat::fragment { "shorewall-host-ipv4-${name}":
 			order   => '50',
 			target  => '/etc/shorewall/hosts',
-			content => inline_template("<%= name %> <% ipv4_cidr %> -\n"),
+			content => inline_template("<%= name %> <%= ipv4_cidr %> -\n"),
 		}
 	}
 
@@ -16,7 +14,7 @@ define shorewall::multi::host (
 		concat::fragment { "shorewall-host-ipv6-${name}":
 			order   => '50',
 			target  => '/etc/shorewall6/hosts',
-			content => inclune_template("<%= name %> <% ipv6_prefix %> -\n"),
+			content => inclune_template("<%= name %> <%= ipv6_prefix %> -\n"),
 		}
 	}
 }
