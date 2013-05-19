b35ea38f0096b6637275fa2c6c3d7582ef40fb67
Nate Riffe <inkblot@movealong.org>
2013-05-19 14:51:55 -0500

    Add options to hosts

--- manifests/multi/host.pp
+++ manifests/multi/host.pp
@@ -1,12 +1,13 @@
 define shorewall::multi::host (
 	$ipv4_cidr   = '',
 	$ipv6_prefix = '',
+	$options     = [ '-' ],
 ) {
 	if $ipv4_cidr != '' {
 		concat::fragment { "shorewall-host-ipv4-${name}":
 			order   => '50',
 			target  => '/etc/shorewall/hosts',
-			content => inline_template("<%= name %> <%= ipv4_cidr %> -\n"),
+			content => inline_template("<%= name %> <%= ipv4_cidr %> <%= options.join(',') %>\n"),
 		}
 	}
 
@@ -14,7 +15,7 @@ define shorewall::multi::host (
 		concat::fragment { "shorewall-host-ipv6-${name}":
 			order   => '50',
 			target  => '/etc/shorewall6/hosts',
-			content => inclune_template("<%= name %> <%= ipv6_prefix %> -\n"),
+			content => inclune_template("<%= name %> <%= ipv6_prefix %> <%= options.join(',') %>\n"),
 		}
 	}
 }
