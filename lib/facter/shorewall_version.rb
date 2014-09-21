839989f96d0c3a67286a7dee873706dc9d4cb177
root <mike+github@renf.ro>
2014-09-21 18:41:26 -0500

    Make shorewall_version fact fail gracefully if shorewall not installed

--- lib/facter/shorewall_version.rb
+++ lib/facter/shorewall_version.rb
@@ -1,16 +1,20 @@
 require 'facter'
-Facter.add(:shorewall_version) do
-  version = 0
-  Facter::Util::Resolution.exec('shorewall version').split('.', 3).each do |v|
-     version = version * 100 + v.to_i
+if File.exists?("/sbin/shorewall")
+  Facter.add(:shorewall_version) do
+    version = 0
+    Facter::Util::Resolution.exec('shorewall version').split('.', 3).each do |v|
+      version = version * 100 + v.to_i
+    end
+    setcode { version }
   end
-  setcode { version }
 end
 require 'facter'
-Facter.add(:shorewall6_version) do
-  version = 0
-  Facter::Util::Resolution.exec('shorewall6 version').split('.', 3).each do |v|
-     version = version * 100 + v.to_i
+if File.exists?("/sbin/shorewall")
+  Facter.add(:shorewall6_version) do
+    version = 0
+    Facter::Util::Resolution.exec('shorewall6 version').split('.', 3).each do |v|
+      version = version * 100 + v.to_i
+    end
+    setcode { version }
   end
-  setcode { version }
 end
