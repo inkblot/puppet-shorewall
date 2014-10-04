5bbfa36147f3ddc3231c0485f036cab36c06af88
Nate Riffe <inkblot@movealong.org>
2014-10-04 10:10:00 -0500

    Make shorewall_version default to 0
    Reverse the nesting of the Facter.add and the if blocks so that the version
    variable's default value of 0 ends up being the fact value, and the fact always
    exists.

--- lib/facter/shorewall_version.rb
+++ lib/facter/shorewall_version.rb
@@ -1,20 +1,20 @@
 require 'facter'
-if File.exists?("/sbin/shorewall")
-  Facter.add(:shorewall_version) do
-    version = 0
+Facter.add(:shorewall_version) do
+  version = 0
+  if File.exists?("/sbin/shorewall")
     Facter::Util::Resolution.exec('shorewall version').split('.', 3).each do |v|
       version = version * 100 + v.to_i
     end
-    setcode { version }
   end
+  setcode { version }
 end
 require 'facter'
-if File.exists?("/sbin/shorewall6")
-  Facter.add(:shorewall6_version) do
-    version = 0
+Facter.add(:shorewall6_version) do
+  version = 0
+  if File.exists?("/sbin/shorewall6")
     Facter::Util::Resolution.exec('shorewall6 version').split('.', 3).each do |v|
       version = version * 100 + v.to_i
     end
-    setcode { version }
   end
+  setcode { version }
 end
