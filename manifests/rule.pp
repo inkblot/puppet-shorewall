b6a870b969dfb22e945cfe20725c581b3cb5b09f
Nate Riffe <inkblot@movealong.org>
2015-01-25 20:09:32 -0600

    ICMP is a protocol, too.

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -14,7 +14,7 @@ define shorewall::rule (
     $order         = '50',
 ) {
     if $application == '' {
-        validate_re($proto, '^(([0-9]+|tcp|udp|-)(?:,|$))+')
+        validate_re($proto, '^(([0-9]+|tcp|udp|icmp|-)(?:,|$))+')
         validate_re($port, ['^[0-9]+$', '^-$', '^[0-9]+:[0-9]+$'])
     } else {
         validate_re($application, '^[[:alnum:]]+$')
